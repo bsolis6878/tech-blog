c6f963a31077daed43fd1595e76915ad8f0c064a
bsolis6878 <bsolis6878@gmail.com>
2022-10-16 18:54:02 -0500

    implemented cookies and api routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 const routes = require('./controllers');
+const session = require('express-session');
+
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
@@ -12,12 +14,26 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-// });
+});

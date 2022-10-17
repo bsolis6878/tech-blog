df6c5a5950cea737dd00fe435f49161339a7dcb3
bsolis6878 <bsolis6878@gmail.com>
2022-10-16 19:30:51 -0500

    added login functionality

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,27 @@
+async function login(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#login-username').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 async function signUp(event) {
     event.preventDefault();
 
@@ -30,4 +54,5 @@ async function signUp(event) {
     }
 };
 
+document.querySelector('.login-form').addEventListener('submit', login);
 document.querySelector('.signup-form').addEventListener('submit', signUp);
